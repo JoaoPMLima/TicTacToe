099f63d6f2b31ce2c6ea1bddb6df4e05d1f1f1fd
JoaoPMLima <62726968+JoaoPMLima@users.noreply.github.com>
2020-04-05 18:43:34 -0300

    Update

--- TicTacToe/MakeGrid.js
+++ TicTacToe/MakeGrid.js
@@ -4,6 +4,19 @@ function MakeGrid(rows){
   this.grid = [];
   this.weigths = [];
 
+  //division 6x6 indexes
+  this.divInd = [];
+  for(let i = 0; i <= floor(this.rows/3); i++){
+    for(let j = 0; j <= floor(this.rows/3); j++){
+      let obj = {
+        x : i,
+        y : j
+      }
+      this.divInd.push(obj);
+    }
+  }
+
+  //console.log(this.divInd);
 
   //construct grid
   for(let i = 0; i < rows; i++){
@@ -16,69 +29,74 @@ function MakeGrid(rows){
   }
 
 
-
   //construct weigths grid
   this.a = -1;
   this.b = -1;
-  this.weigths = new Array(this.rows).fill(new Array(this.rows).fill(1).map(() => {
+
+  this.weigths = new Array(6).fill(new Array(6).fill(1).map(() => {
     this.a++;
     return Math.pow(2,this.a);
   })).map(x => {
     this.b++;
-    return x.map(y => y*Math.pow(2,(this.b*this.rows)));
+    return x.map(y => y*Math.pow(2,(this.b*6)));
   });
 
-  //console.table(this.weigths);
 
-  //construct winConds array
+
   this.winConds = [];
-  let a = 0;
-  //horizontal
-  for(let i = 0; i < this.rows; i++){
-    for(let j = 0; j < this.rows - 2; j++){
-      a = 0;
-      a += this.weigths[i][j];
-      a += this.weigths[i][j+1];
-      a += this.weigths[i][j+2];
-      this.winConds.push(a);
+  //loop divInd
+
+  for(let dontloop = 0; dontloop < 1; dontloop++){
+    let emptyArray = [];
+    //construct winConds array
+
+    let a = 0;
+    //horizontal
+    for(let i = 0; i < 6; i++){
+      for(let j = 0; j < 4; j++){
+        a = 0;
+        a += this.weigths[i][j];
+        a += this.weigths[i][j+1];
+        a += this.weigths[i][j+2];
+        this.winConds.push(a);
+      }
     }
-  }
-  //vertical
-  for(let i = 0; i < this.rows; i++){
-    for(let j = 0; j < this.rows - 2; j++){
-      a = 0;
-      a += this.weigths[j][i];
-      a += this.weigths[j+1][i];
-      a += this.weigths[j+2][i];
-      this.winConds.push(a);
+    //vertical
+    for(let i = 0; i < 6; i++){
+      for(let j = 0; j < 4; j++){
+        a = 0;
+        a += this.weigths[j][i];
+        a += this.weigths[j+1][i];
+        a += this.weigths[j+2][i];
+        this.winConds.push(a);
+      }
     }
-  }
-  //diagonal down
-  for(let i = 0; i < this.rows - 2; i++){
-    for(let j = 0; j < this.rows - 2; j++){
-      a = 0;
-      a += this.weigths[i][j];
-      a += this.weigths[i+1][j+1];
-      a += this.weigths[i+2][j+2];
-      this.winConds.push(a);
+    //diagonal down
+    for(let i = 0; i < 4; i++){
+      for(let j = 0; j < 4; j++){
+        a = 0;
+        a += this.weigths[i][j];
+        a += this.weigths[i+1][j+1];
+        a += this.weigths[i+2][j+2];
+        this.winConds.push(a);
+      }
     }
-  }
-  //diagonal up
-  for(let i = 2; i < this.rows; i++){
-    for(let j = 0; j < this.rows - 2; j++){
-      a = 0;
-      a += this.weigths[i][j];
-      a += this.weigths[i-1][j+1];
-      a += this.weigths[i-2][j+2];
-      this.winConds.push(a);
+    //diagonal up
+    for(let i = 2; i < 6; i++){
+      for(let j = 0; j < 4; j++){
+        a = 0;
+        a += this.weigths[i][j];
+        a += this.weigths[i-1][j+1];
+        a += this.weigths[i-2][j+2];
+        this.winConds.push(a);
+      }
     }
+    //sorting
+    this.winConds.sort((a,b) => a - b);
   }
-  //sorting
-  this.winConds.sort((a,b) => a - b);
 
 
-
-
+  //function to set grid value
   this.setValue = function(i, j, value){
     this.grid[i][j] = value;
   }
--- TicTacToe/sketch.js
+++ TicTacToe/sketch.js
@@ -1,5 +1,13 @@
-let preDraw = true;
+/* If preDraw is set to true the program will start with the values already put in:
+   5x5 grid
+   2 players : 'X' and 'O'
+*/
+let preDraw = false;
 let firstGo = true;
+
+let rowsDefault = 5;
+let numPDefault = 2;
+let playersDefault = ["X","O"];
 
 let done = false;
 let drawn = false;
@@ -13,11 +21,24 @@ let curPlayer = 0;
 let grid;
 
 
+/*
+  DISCLAIMER: If you're reading this, you're probably planning on going through
+this code to try and understand it. Don't. Here's the thing, I started this with
+the idea of doing this by using the binary operator 'and'('&'). As I progressed
+I realized that for any grid bigger than 7x7 the integers would get too high and
+stop working. At that point I could've scratched that whole idea and just made
+some for loops to test every possibility every time, but I didn't. I wanted to
+use the 'and' operator, and here's the horrible (but functional) setup I came
+up with.
+*/
+
+
 function setup() {
   // put setup code here
   if(firstGo){
     createElement('h1','Tic Tac Toe');
   } else {
+    preDraw = true;
     players = [];
     playerScores = [];
     curPlayer = 0;
@@ -28,7 +49,7 @@ function setup() {
   canvas = createCanvas(500,500);
   background(200);
   if(preDraw){
-    rows = 5;
+    rows = rowsDefault;
   } else{
     rows = parseInt(prompt("Insert the number of rows","5"));
   }
@@ -39,33 +60,54 @@ function setup() {
   drawn = true;
 
   //input player symbols
+  let emptyArray = [];
+  //console.log(emptyArray);
   if(preDraw){
-    numPlayers = 3;
-    players.push("X");
-    players.push("O");
-    players.push("A");
-    playerScores.push(0);
-    playerScores.push(0);
-    playerScores.push(0);
+    numPlayers = numPDefault;
+    players = playersDefault;
+    //players.push("A");
+    for(let i = 0; i < numPlayers; i++){
+      playerScores.push(emptyArray.slice());
+    }
+    //playerScores.push(emptyArray.slice());
   } else {
     numPlayers = parseInt(prompt("Insert the number of players","2"));
 
     for(let i = 0; i < numPlayers; i++){
-      let temp = prompt("Insert the player's symbol",(i == 0) ? "X" : "O");
+      let temp = prompt("Insert the player's symbol",(i == 0) ? "X" : (i == 1) ? "O" : ("" + (i-1)));
       if(temp != null){
         players.push(temp);
-        playerScores.push(0);
+        playerScores.push(emptyArray.slice());
       } else {
         i--;
       }
     }
   }
+  for(let i = 0; i < playerScores.length; i++){
+    //console.log("11");
+    for(let j = 0; j < grid.divInd.length; j++){
+      //console.log("12");
+      let tempp = {
+        x : grid.divInd[j].x,
+        y : grid.divInd[j].y,
+        z : 0
+      }
+      playerScores[i].push(tempp);
+    }
+    //console.log(playerScores);
+  }
+  //console.log(grid.winConds);
+  //console.table(grid.weigths);
+
+  if(!preDraw){
+    playersDefault = players;
+    numPDefault = numPlayers;
+    rowsDefault = rows;
+  }
 }
 
 function draw() {
-  // put drawing code here
   background(200);
-  // text(rows,200,200);
   stroke(0);
   textSize(40);
   textAlign(CENTER,CENTER)
@@ -90,25 +132,87 @@ function mousePressed(){
     let y = floor(mouseX/size);
     let x = floor(mouseY/size);
     if(x >= 0 && x < rows && y >=0 && y < rows){
+      //note: x and y are switched here, so they are again in some other parts of this function
       let tempString = grid.grid[x][y];
       if(tempString == ""){
         grid.setValue(x,y,players[curPlayer]);
-        playerScores[curPlayer]+=grid.weigths[x][y];
+
+
+        xx = (x-x%3)/3;
+        yy = (y-y%3)/3;
+        //test for normal index
+        for(let i = 0; i < playerScores[curPlayer].length; i++){
+          if(xx == playerScores[curPlayer][i].x && yy == playerScores[curPlayer][i].y){
+            //console.log("aiaiai");
+            //console.log(grid.weigths[y%3][x%3]);
+            //x and y are switched here
+            playerScores[curPlayer][i].z+=grid.weigths[y%3][x%3];
+            break;
+          }
+        }
+        //test for previous x index if exists
+        if(xx > 0){
+          //console.log("vai 2");
+          //console.log(xx-1);
+          //console.log(yy);
+          for(let i = 0; i < playerScores[curPlayer].length; i++){
+            if(xx-1 == playerScores[curPlayer][i].x && yy == playerScores[curPlayer][i].y){
+              //console.log("aiaiai2");
+              //console.log(grid.weigths[y%3][x%3+3]);
+              //x and y are switched here
+              playerScores[curPlayer][i].z+=grid.weigths[y%3][x%3+3];
+              break;
+            }
+          }
+        }
+        //do the same thing again for previous y index if exists
+        if(yy>0){
+          //console.log("vai 3");
+          for(let i = 0; i < playerScores[curPlayer].length; i++){
+            if(xx == playerScores[curPlayer][i].x && yy-1 == playerScores[curPlayer][i].y){
+              //console.log("aiaiai3");
+              //console.log(grid.weigths[y%3+3][x%3]);
+              //x and y are switched here
+              playerScores[curPlayer][i].z+=grid.weigths[y%3+3][x%3];
+              break;
+            }
+          }
+          if(xx > 0){
+            //console.log("vai 4");
+            for(let i = 0; i < playerScores[curPlayer].length; i++){
+              if(xx-1 == playerScores[curPlayer][i].x && yy-1 == playerScores[curPlayer][i].y){
+                //console.log("aiaiai4");
+                //console.log(grid.weigths[y%3+3][x%3+3]);
+                //x and y are switched here
+                playerScores[curPlayer][i].z+=grid.weigths[y%3+3][x%3+3];
+                break;
+              }
+            }
+          }
+        }
         curPlayer++;
         curPlayer = curPlayer % numPlayers;
       }
+      //console.log("Value for that point: "+grid.weigths[y][x]);
     }
 
+loop1:
+//test z for matching with any winCondition for every item in every array in the playerScores array
     for(let i = 0; i < playerScores.length; i++){
-      for(let winCond of grid.winConds){
-        if((playerScores[i] & winCond) == winCond){
-          console.log("1");
-          displayWinner(i);
-          break;
+      for(let p of playerScores[i]){
+        for(let winCond of grid.winConds){
+          if((p.z & winCond) == winCond){
+            //console.log(p);
+            //console.log(winCond);
+            displayWinner(i);
+            break loop1;
+          }
         }
       }
     }
+
     checkIfLost();
+    //console.log(playerScores);
   }
 }
 
@@ -130,31 +234,10 @@ function displayWinner(index){
 }
 
 function checkIfLost(){
-  if(playerScores.reduce((a,b) => a + b) == Math.pow(2,rows*rows) - 1){
-    displayWinner(NaN);
+  for(let i = 0; i < grid.rows; i++){
+    for(let j = 0; j < grid.rows; j++){
+      if(grid.grid[i][j] == "") return;
+    }
   }
+  displayWinner(NaN);
 }
-
-
-
-
-
-
-/* Em seguida:
-
-Done:
-- transformar o grid em uma classe separada
-- array de jogadores
-- funcao proximo jogador
-- array winConditions
-- array pontuacao dos jogadores
-
-Pending:
-
-- comparar sempre pra ver winConditions pra cada jogador
-- loseCondition
-
-
-
-
-*/
